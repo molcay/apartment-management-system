93a73b0005b99cb04093905372ccceb92b2e76cb
Muhammed Olcay TERCANLI <m.olcay25@gmail.com>
2019-12-15 19:49:56 +0300

    Add related serializers for djoser

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
+from rest_framework.validators import UniqueValidator
 
 from api.models import Guarantor, Tenant, Landlord, Room, Agreement
 
@@ -43,3 +46,32 @@ class AgreementSerializer(AgreementUpdateSerializer):
     room = RoomSerializer()
     tenant = TenantSerializer()
     guarantor = GuarantorSerializer()
+
+
+class UserRegistrationSerializer(BaseUserRegistrationSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[
+            UniqueValidator(queryset=User.objects.all())
+        ]
+    )
+
+    def validate_username(self, value: str):
+        if not validate_tc(value):
+            raise serializers.ValidationError('Username must be T.C. ID.')
+        return value
+
+
+def validate_tc(value):
+    if not (len(value) == 11 and value.isdigit() and value[0] != '0' and value[-1] in '02468'):
+        return False
+    digits = [int(c) for c in value]
+    tenth_digit = ((sum(digits[:9:2]) * 7) - (sum(digits[1:9:2]))) % 10
+    is_tenth_digit_valid = tenth_digit == digits[9]
+    if not is_tenth_digit_valid:
+        return False
+    eleventh_digit = sum(digits[:-1]) % 10
+    is_eleventh_digit_valid = eleventh_digit == digits[-1]
+    if not is_eleventh_digit_valid:
+        return False
+    return True
